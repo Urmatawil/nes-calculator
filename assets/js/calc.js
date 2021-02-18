a8fcbddc4252f00d015a96fe176ad5df51e667f0
urmatawil <susurrus.ater@gmail.com>
2021-02-17 23:56:18 -0300

    nes update, delete old files

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -4,60 +4,32 @@ Vue.createApp({
   data() {
     return {
       keyInput: "",
-      total: "",
-      showTotal: false,
-      operation: "",
       operators: ["/", "*", "-", "+"],
       keys: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-      keyScr: "",
+      result: "",
+      showTotal: false,
     };
   },
 
   computed: {
     showScreen: function () {
-      if (this.showTotal) return this.total;
+      if (this.showTotal) return this.result;
       else return this.keyInput === "" ? "0" : this.keyInput;
     },
   },
 
   methods: {
-    numberScreen: function (e) {
-      if (this.showTotal) this.showTotal = false;
+    calculate: function (e) {
       this.keyInput += e.target.value;
     },
 
-    operationScreen: function (e) {
-      this.operation = e.target.value;
-      this.keyScr = this.keyInput;
+    equal: function () {
+      parseFloat((this.result = eval(this.keyInput)));
       this.keyInput = "";
-    },
-
-    calculate: function (n1, op, n2) {
-      switch (op) {
-        case "+":
-          return parseFloat(n1) + parseFloat(n2);
-        case "-":
-          return parseFloat(n1) - parseFloat(n2);
-        case "*":
-          return parseFloat(n1) * parseFloat(n2);
-        case "/":
-          return parseFloat(n1) / parseFloat(n2);
-      }
-    },
-
-    equalFunction: function (e) {
-      this.total = this.calculate(this.keyScr, this.operation, this.keyInput);
-      this.operation = "";
-      this.keyInput = "";
-      this.showTotal = true;
     },
 
     clear: function () {
-      this.keyInput = "";
-      this.total = "";
-      this.keyScr = "";
-      this.operation = "";
-      this.showTotal = false;
+      (this.keyInput = ""), (this.result = ""), (this.showTotal = false);
     },
   },
 }).mount("#app");
